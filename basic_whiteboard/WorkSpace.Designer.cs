651bccc3d27fd901f0e91ef145026d7a6659fd44
blee203 <bichlybichly.0@gmail.com>
2023-06-18 20:18:06 +0700

    first commit

---
+++ basic_whiteboard/WorkSpace.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace basic_whiteboard
+{
+    partial class WorkSpace
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(46, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(150, 31);
+            this.textBox1.TabIndex = 0;
+            // 
+            // WorkSpace
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Name = "WorkSpace";
+            this.Text = "WorkSpace";
+            this.Load += new System.EventHandler(this.WorkSpace_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ basic_whiteboard/WorkSpace.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+namespace basic_whiteboard
+{
+    public partial class WorkSpace : Form
+    {
+        public string code;
+        public string name;
+        public string _port = "9090";
+        public WorkSpace()
+        {
+            InitializeComponent();
+        }
+        public void funData(string str, string text, string port)
+        {
+            code = str;
+            name = text;
+            _port = port;
+            Connect();
+        }
+
+        IPEndPoint IP;
+        Socket Client;
+
+        void Connect()
+        {
+            //IP: server address
+            int Port = Int32.Parse(_port);
+            IP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), Port);
+            Client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+
+            try
+            {
+                Client.Connect(IP);
+            }
+            catch
+            {
+                MessageBox.Show("Can't connect", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Environment.Exit(0);
+                return;
+            }
+
+            Thread listener = new Thread(Receive);
+            listener.IsBackground = true;
+            listener.Start();
+        }
+
+        void Receive()
+        {
+
+        }
+
+        private void WorkSpace_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = code;
+        }
+    }
+}
